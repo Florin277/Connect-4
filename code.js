678e0c265017e601d52b36ab2a521a06f7a4a437
Florin277 <simoncaflorin79@gmail.com>
2025-06-10 18:39:27 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -47,49 +47,65 @@ function cellReceivesColor(pressPos) {
     }
     cellStaining(pressPos, cellColor);
     if (numberOfClicks >= startCheck) {
-        checkWinner(cellColor);
+        checkWinner();
     }
     ++numberOfClicks;
 }
-let idemColorCell = 1;
 
-function checkWinner(cellColor) {
+function checkWinner() {
+    if (checkLine() || checkColumn() || diagonalCheck()) {
+        printMessage(cellColor);
+    }
+}
+
+function checkLine() {
+    let idemColorCell = 1;
     for (let i = lineNo; i > 0 && haveWinner === 0; --i) {
         for (let j = 1; j < columnNo; ++j) {
             let currCellColor = document.getElementById(i * TEN + j).style.backgroundColor;
+            let nextCellColor = document.getElementById(i * TEN + j + 1).style.backgroundColor;
             if(currCellColor !== "rgb(185, 255, 127)") {
-                if (document.getElementById(i * TEN + j).style.backgroundColor === 
-                    document.getElementById(i * TEN + j + 1).style.backgroundColor) {
+                if (currCellColor === nextCellColor) {
                     ++idemColorCell;
                 } else {
                     idemColorCell = 1;
                 }
                 if (idemColorCell === 4) {
-                    printMessage(cellColor);
+                    
                     haveWinner = 1;
+                    break;
                 }
             }
         }
         idemColorCell = 1;
     }
+    return haveWinner;
+}
+
+function checkColumn() {
+    let idemColorCell = 1;
     for (let j = 1; j <= columnNo && haveWinner === 0; ++j) {
         for (let i = lineNo; i > 1; --i) {
             let currCellColor = document.getElementById(i * TEN + j).style.backgroundColor;
+            let nextCellColor = document.getElementById((i - 1) * TEN + j).style.backgroundColor;
             if(currCellColor !== "rgb(185, 255, 127)") {
-                if (document.getElementById(i * TEN + j).style.backgroundColor === 
-                    document.getElementById((i - 1) * TEN + j).style.backgroundColor) {
+                if (currCellColor === nextCellColor ) {
                     ++idemColorCell;
                 } else {
                     idemColorCell = 1;
                 }
                 if (idemColorCell === 4) {
-                    printMessage(cellColor);
                     haveWinner = 1;
                 }
             }
         }
         idemColorCell = 1;
     }
+    return haveWinner;
+}
+
+function diagonalCheck() {
+    let idemColorCell = 1;
     let idemColorCell2 = 1;
     let firstColumn = 1;
     let lastColumn = columnNo;
@@ -102,9 +118,9 @@ function checkWinner(cellColor) {
         }
         for (let j1 = firstColumn, j2 = lastColumn; j1 < columnNo && line > 1 && haveWinner === 0; ++j1, --j2, --line) {
             let currCellColor1 = document.getElementById(line * TEN + j1).style.backgroundColor;
-            let nextCellColor1 = document.getElementById((line - 1) * TEN + (j1 + 1)).style.backgroundColor;
+            let nextCellColor1 = document.getElementById((line - 1) * TEN + j1 + 1).style.backgroundColor;
             let currCellColor2 = document.getElementById(line * TEN + j2).style.backgroundColor;
-            let nextCellColor2 = document.getElementById((line - 1) * TEN + (j2 - 1)).style.backgroundColor;
+            let nextCellColor2 = document.getElementById((line - 1) * TEN + j2 - 1).style.backgroundColor;
             if (currCellColor1 !== "rgb(185, 255, 127)") {
                 if(currCellColor1 === nextCellColor1) {
                     ++idemColorCell;
@@ -120,20 +136,20 @@ function checkWinner(cellColor) {
                 }
             } 
             if (idemColorCell === 4 || idemColorCell2 === 4) {
-                printMessage(cellColor);
                 haveWinner = 1;
             }
         }  
         idemColorCell = 1;  
         idemColorCell2 = 1;
     }
+    return haveWinner;
 }
-    
+
 function cellStaining (pressPos, cellColor) {
     let cellId = parseInt(pressPos);
     let nextPosId = cellId + TEN;
     if (document.getElementById(nextPosId).style.backgroundColor !== "rgb(185, 255, 127)") {
-        document.getElementById(nextPosId - TEN).style.backgroundColor = cellColor;
+        document.getElementById(cellId).style.backgroundColor = cellColor;
     }
     while(nextPosId <= lastPos && document.getElementById(nextPosId).style.backgroundColor === "rgb(185, 255, 127)") {
         document.getElementById(nextPosId).style.backgroundColor = cellColor;
